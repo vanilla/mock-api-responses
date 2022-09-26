86e648d5026d1584153a6b739f86ac420e0843cf
taylorchance <taylorchance@gmail.com>
2022-09-26 12:23:05 -0400

    adding TaylorC

--- vanilla-search-api/src/Utils/Employees.php
+++ vanilla-search-api/src/Utils/Employees.php
@@ -43,6 +43,7 @@ final class Employees {
         24000 => 'mgriffhl',
         25000 => 'AndrewK',
         26000 => 'SoorajF',
-        27000 => 'JennyS'
+        27000 => 'JennyS',
+        28000 => 'TaylorC'
     ];
 }
